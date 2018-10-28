580c524d4a344ea93bac70659453d577e1dc623b
SHB-0328\hyebeen <shb0328@gmail.com>
2018-10-28 23:59:18 +0900

    필요없는 주석 제거 및 include 제거

--- chainedMatrixMult_Godbole/minmult.cpp
+++ chainedMatrixMult_Godbole/minmult.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using std::endl;
 using std::cout;
-using std::min;
-using std::min_element;
 
 #define N 6
 
 int minmult(int n, const int d[N], int P[N][N]) ; 
+void order(int i,int j,const int P[N][N]) ;
 
 int main() {
 
 	int d[N+1] = {5,2,3,4,6,7,8};//7
 	int P[N][N];
 	int res = minmult(N,d,P);
-	cout<<"result = "<<res<<endl;
+	cout<<"Minimum Number of Multiplications = "<<res<<endl;
+	order(1,6,P);
 	return 0;
 }
 
@@ -34,16 +32,23 @@ int minmult(const int n, const int d[N], int P[N][N])
 	for(diagonal = 1; diagonal <= n-1; ++diagonal){
 		for(i = 0; i< n - diagonal; ++i){
 			j = i + diagonal;
-
-			int temp[diagonal];
-			for(k = i; k <= j-1; ++k)
-				temp[k-i] = M[i][k] + M[k+1][j] + d[i]*d[k+1]*d[j+1];
 			
-			M[i][j] = *min_element(temp,temp+diagonal);
+			for(k = i; k <= j-1; ++k){
+				if(k == i){
+					P[i][j] = k+1; 
+					M[i][j] = M[i][k] + M[k+1][j] + d[i]*d[k+1]*d[j+1];
+				}
+				else{
+					if(M[i][j] > M[i][k] + M[k+1][j] + d[i]*d[k+1]*d[j+1]){
+						P[i][j] = k+1;
+						M[i][j] = M[i][k] + M[k+1][j] + d[i]*d[k+1]*d[j+1];
+					}
+				}
+			}
+			
 			cout<<endl<<"M["<<i<<"]["<<j<<"] = "<<M[i][j]<<endl;
 		}
 	}
-
 
 	cout<<endl;
 	for(int i =0; i<n;++i){
@@ -54,4 +59,16 @@ int minmult(const int n, const int d[N], int P[N][N])
 	cout<<endl;
 		
 	return M[0][n-1];
+}
+
+void order(int i,int j,const int P[N][N]) 
+{
+	if(i == j) cout<<"A"<<i<<"";
+	else {
+		int k = P[i-1][j-1];
+		cout<<"(";
+		order(i,k,P);
+		order(k+1,j,P);
+		cout<<")";
+	}
 }
