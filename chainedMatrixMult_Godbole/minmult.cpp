95d7fe931940f57d4b456f1560760c70e461e622
SHB-0328\hyebeen <shb0328@gmail.com>
2018-10-28 21:11:46 +0900

    불필요한 주석 및 에러체크용 출력문 전부 제거

--- chainedMatrixMult_Godbole/minmult.cpp
+++ chainedMatrixMult_Godbole/minmult.cpp
@@ -15,10 +15,6 @@ int main() {
 
 	int d[N+1] = {5,2,3,4,6,7,8};//7
 	int P[N][N];
-	// for(int i = 0 ; i < N ; ++i) {
-	// 	for(int j = 0; j < N; ++j)
-	// 		P[i][j] = 0;
-	// }
 	int res = minmult(N,d,P);
 	cout<<"result = "<<res<<endl;
 	return 0;
@@ -35,42 +31,27 @@ int minmult(const int n, const int d[N], int P[N][N])
 			P[i][j] = 0;
 	}
 
-	for(int i =0; i<n;++i){
-		for(int j =0; j<n; ++j)
-			cout<<M[i][j]<<" ";
-		cout<<endl;
-	}
-		
-
 	for(diagonal = 1; diagonal <= n-1; ++diagonal){
 		for(i = 0; i< n - diagonal; ++i){
 			j = i + diagonal;
+
 			int temp[diagonal];
-			//std::vector<int> temp;
-			for(k = i; k <= j-1; ++k){
-				cout<<"i = "<<i<<", k = "<<k<<", j = "<<j<<endl;
-				cout<<"M[i,k] = "<<M[i][k]<<", M[k+1,j] = "<<M[k+1][j]<<", d[i]d[k+1]d[j+1] = "<<d[i]*d[k+1]*d[j+1]<<endl;
+			for(k = i; k <= j-1; ++k)
 				temp[k-i] = M[i][k] + M[k+1][j] + d[i]*d[k+1]*d[j+1];
-				cout<<"temp[k-i] = "<<temp[k-i]<<endl;
-				//temp.push_back(M[i][k] + M[k+1][j] + d[i-1]*d[k]*d[j]);
-				// int temp = M[i][k] + M[k+1][j] + d[i-1]*d[k]*d[j];
-				// if(M[i][j]>temp)
-				// 	M[i][j] = temp;
-			}
-			for(int q = 0; q < sizeof(temp) ; ++q)
-					cout<<temp[q]<<" ";
-				cout<<endl;
-				M[i][j] = *min_element(temp,temp+diagonal);
-			//M[i][j] = *min_element(temp.begin(),temp.end());
-			// M[i][j] = *min_element(temp,temp+diagonal);
+			
+			M[i][j] = *min_element(temp,temp+diagonal);
 			cout<<endl<<"M["<<i<<"]["<<j<<"] = "<<M[i][j]<<endl;
 		}
 	}
 
+
+	cout<<endl;
 	for(int i =0; i<n;++i){
 		for(int j =0; j<n; ++j)
 			cout<<M[i][j]<<" ";
 		cout<<endl;
 	}
+	cout<<endl;
+		
 	return M[0][n-1];
 }
