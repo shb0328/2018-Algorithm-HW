86aed9668acb3a0e29d0cc133c5a23331db89714
SHB-0328\hyebeen <shb0328@gmail.com>
2018-10-29 01:25:12 +0900

    P도 출력

--- chainedMatrixMult_Godbole/minmult.cpp
+++ chainedMatrixMult_Godbole/minmult.cpp
@@ -19,6 +19,8 @@ int main() {
 	cout<<endl<<endl;
 	
 	int res = minmult(N,d,P);
+
+	
 	cout<<"Minimum Number of Multiplications = "<<res<<endl;
 	order(1,6,P);
 	cout<<endl;
@@ -51,10 +53,9 @@ int minmult(const int n, const int d[N], int P[N][N])
 						M[i][j] = M[i][k] + M[k+1][j] + d[i]*d[k+1]*d[j+1];
 					}
 				}
-			}
-		}
-	}
+			}}}
 
+	//output
 	cout<<"M = "<<endl;
 	for(int i =0; i<n;++i){
 		for(int j =0; j<n; ++j)
@@ -62,7 +63,16 @@ int minmult(const int n, const int d[N], int P[N][N])
 		cout<<endl;
 	}
 	cout<<endl;
-		
+	
+	cout<<"P = "<<endl;
+	for(int i = 0;i<N;i++){
+		for (int j = 0; j < N; ++j)
+			cout<<P[i][j]<<" ";
+		cout<<endl;
+	}
+	cout<<endl;	
+	//
+
 	return M[0][n-1];
 }
 
