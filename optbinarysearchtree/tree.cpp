db7daa8f53d6535ab76309d014c1001ea6ffd0c5
SHB-0328\hyebeen <shb0328@gmail.com>
2018-11-01 21:00:45 +0900

    최종 제출용 코드

--- optbinarysearchtree/tree.cpp
+++ optbinarysearchtree/tree.cpp
@@ -35,8 +35,6 @@ int main() {
 
 	node* root = tree(1,N,R);
 
-	cout<<root->key<<endl;
-
 	cout<<"Minimum Average = "<<minavg<<endl;
 	return 0;
 }
@@ -44,19 +42,19 @@ int main() {
 
 node* tree(int i, int j, const int R[N+1][N+1])
 {
-	cout<<"tree("<<i-1<<", "<<j<<")"<<endl;
+	// cout<<"tree("<<i-1<<", "<<j<<")"<<endl;
 	int k;
 	node * p;
-	cout<<"R["<<i-1<<"]["<<j<<"] = "<<R[i-1][j]<<endl;
+	// cout<<"R["<<i-1<<"]["<<j<<"] = "<<R[i-1][j]<<endl;
 	k = R[i-1][j];
 	if(k <= 0){
-		cout<<"return null"<<endl;
+		// cout<<"return null"<<endl;
 		return NULL;
 	}
 	else {
 		p = new node;
 		p -> key = keys[k-1];
-		cout<<"key = "<<p->key<<endl;
+		// cout<<"key = "<<p->key<<endl;
 		p -> left = tree(i,k-1,R);
 		p -> right = tree(k+1,j,R);
 		return p;
@@ -68,16 +66,14 @@ void optsearchtree(int n, const float p[N], float& minavg, int R[N+1][N+1])
 	int i,j,k,diagonal;
 	float A[n+1][n+1];
 	
-	//
-	for(int i =0; i<=n; ++i){
-		for(int j =0; j<=n; ++j)
-			R[i][j] = 0;
-	}
-	for(int i =0; i<=n; ++i){
-		for(int j =0; j<=n; ++j)
-			A[i][j] = 0;
-	}
 	//init
+	for(int i =0; i<=n; ++i){
+		for(int j =0; j<=n; ++j){
+			R[i][j] = 0;
+			A[i][j] = 0;
+		}	
+	}
+
 	for(i = 1; i <= n; ++i){
 		A[i-1][i-1] = 0;
 		A[i-1][i] = p[i-1];
@@ -85,71 +81,39 @@ void optsearchtree(int n, const float p[N], float& minavg, int R[N+1][N+1])
 		R[i-1][i] = i;
 	} A[n][n] = 0; R[n][n] = 0;
 	//
-	//test
-	for(int i =0; i<=n; ++i){
-		for(int j =0; j<=n; ++j)
-			cout<<R[i][j]<<" ";
-		cout<<endl;
-	}
-	cout<<endl;
-	for(int i =0; i<=n; ++i){
-		for(int j =0; j<=n; ++j)
-			cout<<A[i][j]<<" ";
-		cout<<endl;
-	}
-	cout<<endl;
-	//
-
-
 
 	for(diagonal = 1; diagonal <= n-1; ++diagonal){
 		for(i = 1; i <= n - diagonal; ++i) {
 			j = i + diagonal;
 			for(k = i; k <= j; ++k){
-				cout<<"A["<<i-1<<"]["<<k-1<<"] = "<<A[i-1][k-1]<<", A["<<k<<"]["<<j<<"] = "<<A[k][j]<<", sigma" <<sigma(i-1,j-1,p)<<endl;
 				float tmp = A[i-1][k-1] + A[k][j] + sigma(i-1,j-1,p);
-				//
-				cout<<"di = " <<diagonal<<", i = "<<i<<", j = "<<j<<", tmp = "<<tmp<<endl;
+				
 				if( k == i ) {
 					A[i-1][j] = tmp;
 					R[i-1][j] = k;
-				}
-				else {
+				}else {
 					if(A[i-1][j] > tmp){
 						A[i-1][j] = tmp;
-						R[i-1][j] = k;
-					}
+						R[i-1][j] = k; }
 				}
 
-//test
-				for(int i =0; i<=n; ++i){
-					for(int j =0; j<=n; ++j)
-						cout<<R[i][j]<<" ";
-					cout<<endl;
-				}
-				cout<<endl;
-				for(int i =0; i<=n; ++i){
-					for(int j =0; j<=n; ++j)
-						cout<<A[i][j]<<" ";
-					cout<<endl;
-				}
-				cout<<endl;
-	//
 			}}}
-			//test
+			
+			//show A,R
+			cout<<"A = "<<endl;
+			for(int i =0; i<=n; ++i){
+				for(int j =0; j<=n; ++j)
+					cout<<A[i][j]<<" ";
+				cout<<endl;
+			}
+			cout<<endl<<"R = "<<endl;
 			for(int i =0; i<=n; ++i){
 				for(int j =0; j<=n; ++j)
 					cout<<R[i][j]<<" ";
 				cout<<endl;
 			}
 			cout<<endl;
-			for(int i =0; i<=n; ++i){
-				for(int j =0; j<=n; ++j)
-					cout<<A[i][j]<<" ";
-				cout<<endl;
-			}
-			cout<<endl;
-	//
+			//
 			minavg = A[0][n];
 		}
 
@@ -158,7 +122,6 @@ void optsearchtree(int n, const float p[N], float& minavg, int R[N+1][N+1])
 			float res = 0;
 			for(int i = q; i <= r; ++i){
 				res += p[i];
-				cout<<"res in sigma = "<<res<<endl;
 			}
 			return res;
 		}
